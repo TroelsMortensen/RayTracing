8ebf6060d0a53541bdee180e58035790f95bb4b0
TRMO <tromortensen@hotmail.com>
2024-08-18 16:37:04 +0200

    starting on geometry stuff

---
+++ Core/Core/Geometry.cs
@@ -0,0 +1,20 @@
+﻿namespace Core;
+
+public class Geometry
+{
+       
+}
+
+public record Vec3();
+
+public record Point();
+
+public static class Vector3Exts
+{
+    
+}
+
+public static class PointExts
+{
+    
+}
