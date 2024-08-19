08ea2e70569a82c7d4eb1d1fcf44fbc1e3e1c034
TRMO <tromortensen@hotmail.com>
2024-08-19 09:52:13 +0200

    reorganize code

--- Core/Core/Image.cs
+++ Core/Core/Image.cs
@@ -4,25 +4,15 @@ public record Pixel(float R, float G, float B);
 
 public record Image
 {
-    public Image(int width, int height) =>
-        (Width, Height, Pixels) = (width, height, new Pixel[width * height]);
-    //     Width = width;
-    //     Height = height;
-    //     Pixels = new Pixel[width * height];
-    // }
-
-    public Image(int width, int height, Pixel[] pixels) =>
-        (Width, Height, Pixels) = (width, height, pixels);
-
     public int Width { get; }
     public int Height { get; }
     public Pixel[] Pixels { get; }
 
-    // public void SetPixel(Pixel p, int x, int y) =>
-    //     Pixels[x + Width * y] = p;
+    public Image(int width, int height) =>
+        (Width, Height, Pixels) = (width, height, new Pixel[width * height]);
 
-    // public Pixel GetPixel(int x, int y) =>
-    //     ;
+    public Image(int width, int height, Pixel[] pixels) =>
+        (Width, Height, Pixels) = (width, height, pixels);
 
     public Pixel this[int x, int y]
     {
