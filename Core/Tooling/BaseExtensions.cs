fcbc56a22b75e1eb8578eac177473c01bc18553c
TRMO <tromortensen@hotmail.com>
2024-08-19 12:13:25 +0200

    disable naming inconsistency

--- Core/Tooling/BaseExtensions.cs
+++ Core/Tooling/BaseExtensions.cs
@@ -4,6 +4,7 @@ public static class BaseExtensions
 {
     public static void Finally<T>(this T self, Action<T> action) => action(self);
 
+    // ReSharper disable once InconsistentNaming
     public static R Then<T, R>(this T self, Func<T, R> func) => func(self);
 
     public static string StringJoin(this IEnumerable<string> seq, string separator) =>
