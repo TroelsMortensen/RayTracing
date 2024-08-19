f809dd37c039bf887f62352455d93be1ad44b69c
TRMO <tromortensen@hotmail.com>
2024-08-19 09:52:02 +0200

    add dot and cross functions

--- Core/Core/Geometry.cs
+++ Core/Core/Geometry.cs
@@ -16,13 +16,15 @@ public static class Vector3Exts
     public static Vec3 operator *(Vec3 a, double b) => new(a.X * b, a.Y * b, a.Z * b);
     public static Vec3 operator *(double a, Vec3 b) => new(a * b.X, a * b.Y, a * b.Z);
     public static Vec3 operator /(Vec3 a, double b) => new(a.X / b, a.Y / b, a.Z / b);
+    public static double Dot(Vec3 a, Vec3 b) => a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+    public static Vec3 Cross(Vec3 a, Vec3 b) => new(a.Y * b.Z - a.Z * b.Y, a.Z * b.X - a.X * b.Z, a.X * b.Y - a.Y * b.X);
 }
 
-public static class PointExts
+public static class Point3Exts // May need something here eventually?
 {
 }
 
-public static class SharedExts
+public static class PointToVectorAndBack
 {
     public static implicit operator Vec3(Point p) => new Vec3(p.X, p.Y, p.Z);
     public static implicit operator Point(Vec3 v) => new Point(v.X, v.Y, v.Z);
