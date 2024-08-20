1a3c9d00ce2aa2cfb17b3c0a913723a8cb2d1183
TRMO <tromortensen@hotmail.com>
2024-08-20 12:33:44 +0200

    start on camera

--- Core/Core/Scene.cs
+++ Core/Core/Scene.cs
@@ -2,4 +2,22 @@
 
 public record Scene();
 
-public record Camera();
+public record ViewPort(int Width, int Height);
+
+public record Camera
+{
+    public Point3 Center { get; }
+    public double FocalLength { get; } = 1.0; // distance from cam center to view port center
+
+    public Camera(Point3 center)
+    {
+        Center = center;
+    }
+}
+
+public static class CameraExts
+{
+
+    public static Color RayColor(this Camera camera, Ray ray)
+        => Color.BLACK;
+}
