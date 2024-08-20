891cacd6fe32332bc66785d9f2882b31d666d827
TRMO <tromortensen@hotmail.com>
2024-08-20 19:45:48 +0200

    adding a not currenlty working test

---
+++ UnitTests/PathTracerTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using Core;
+
+namespace UnitTests;
+
+public class PathTracerTests
+{
+    
+    [Fact]
+    public void CanDeserializeImage()
+    {
+        string json = File.ReadAllText(@"C:\TRMO\RiderProjects\RayTracing\Shell\CLI\Json.txt");
+        Image image = JsonSerializer.Deserialize<Image>(json)!;
+        Assert.NotNull(image);
+        Assert.Equal(50, image.Width);
+        Assert.Equal(50*28, image.Colors.Length);
+    }
+    
+}
