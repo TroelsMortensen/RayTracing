486eb3c71004fc2d2bf098526a0cf635ffdd7c55
TRMO <tromortensen@hotmail.com>
2024-08-20 19:46:48 +0200

    clean up?

--- Core/Core/Image.cs
+++ Core/Core/Image.cs
@@ -1,10 +1,24 @@
 ﻿namespace Core;
 
-public record Color(double R, double G, double B)
+public record Color
 {
-    public Color BLACK => new Color(0.0, 0.0, 0.0);
+    private Color()
+    {
+    }
+
+    public Color(double R, double G, double B)
+    {
+        this.R = R;
+        this.G = G;
+        this.B = B;
+    }
+
+    // public static Color BLACK => new Color(0.0, 0.0, 0.0);
     public static Color operator *(double a, Color b) => new(a * b.R, a * b.G, a * b.B);
     public static Color operator +(Color a, Color b) => new(a.R + b.R, a.G + b.G, a.B + b.B);
+    public double R { get; init; }
+    public double G { get; init; }
+    public double B { get; init; }
 
     public override string ToString() =>
         $"Color: ({R}, {G}, {B})";
@@ -20,6 +34,10 @@ public record Image
     public int Width { get; }
     public int Height { get; }
     public Color[] Colors { get; }
+
+    public Image()
+    {
+    }
 
     public Image(int width, int height) =>
         (Width, Height, Colors) = (width, height, new Color[width * height]);
