509a1aa61f3664acffb6285ef9c451392ca0e0ca
TRMO <tromortensen@hotmail.com>
2024-08-22 09:36:28 +0200

    suppress naming inconsistencies

--- Core/Core/GeometryModule.cs
+++ Core/Core/GeometryModule.cs
@@ -18,6 +18,7 @@ public record Vec3(double X, double Y, double Z)
     public static implicit operator Point3(Vec3 v) => new Point3(v.X, v.Y, v.Z);
 }
 
+// ReSharper disable once IdentifierTypo
 public static class Vec3Exts
 {
     public static double Length(this Vec3 v) => Math.Sqrt(v.X.Square() + v.Y.Square() + v.Z.Square());
@@ -31,9 +32,10 @@ public record Point3(double X, double Y, double Z)
     public static Point3 operator -(Point3 a, Point3 b) => new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
 }
 
+// ReSharper disable once IdentifierTypo
 public static class Point3Exts
 {
-    public static Point3 PointZero => new Point3(0.0, 0.0, 0.0);
+    public static Point3 PointZero => new(0.0, 0.0, 0.0);
 }
 
 public record Ray(Point3 Origin, Vec3 Direction);
@@ -47,6 +49,7 @@ public static class ObjectIntersectionExts
 {
     public static bool RayHitsSphere(Point3 center, double radius, Ray r)
     {
+        // This essentially solves a quadratic equation to determine if the ray intersects the sphere
         Vec3 oc = r.Origin - center;
         double a = Dot(r.Direction, r.Direction);
         double b = -2.0 * Dot(r.Direction, oc);
