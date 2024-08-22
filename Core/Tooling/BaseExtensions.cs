722ca5411a5d532894f86a4025c6ee6e17f4d34f
TRMO <tromortensen@hotmail.com>
2024-08-22 09:45:47 +0200

    more naming warnings silenced

--- Core/Tooling/BaseExtensions.cs
+++ Core/Tooling/BaseExtensions.cs
@@ -1,10 +1,10 @@
 ﻿namespace Tooling;
 
+// ReSharper disable InconsistentNaming
 public static class BaseExtensions
 {
     public static void Finally<T>(this T self, Action<T> action) => action(self);
 
-    // ReSharper disable once InconsistentNaming
     public static R Then<T, R>(this T self, Func<T, R> func) => func(self);
 
     public static IEnumerable<R> Then<T, R>(this IEnumerable<T> seq, Func<T, R> func) =>
